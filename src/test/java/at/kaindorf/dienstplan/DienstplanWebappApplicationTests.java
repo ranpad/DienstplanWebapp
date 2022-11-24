91e41491e1a76f3e78bf02066ea5f73c08a2a872
ranpad19 <ranpad19@htl-kaindorf.at>
2022-11-24 15:49:06 +0100

    initial commit

---
+++ src/test/java/at/kaindorf/dienstplan/DienstplanWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kaindorf.dienstplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DienstplanWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
