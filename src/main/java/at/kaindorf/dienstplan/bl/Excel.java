281ea87da2978ab9e9f0dec89c0cd43219b05650
ranpad19 <ranpad19@htl-kaindorf.at>
2023-03-30 18:37:25 +0200

    writeToExcel File Method

--- src/main/java/at/kaindorf/dienstplan/bl/Excel.java
+++ src/main/java/at/kaindorf/dienstplan/bl/Excel.java
@@ -1,6 +1,7 @@
 package at.kaindorf.dienstplan.bl;
 
 import at.kaindorf.dienstplan.pojos.Mitarbeiter;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -10,6 +11,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Excel {
@@ -19,8 +21,11 @@ public class Excel {
 
     public String returnExcelRows() {
         List<List<String>> rows = null;
+        List<String> weekdays = new ArrayList<>();
         List<Mitarbeiter> mitarbeiterList = new ArrayList<>();
         File excelFile = null;
+
+        String[] jsonstring;
 
         try {
             excelFile = new File("src/main/resources/Excel/Wichtige_Dinge_Dienstplan.xlsx");
@@ -32,8 +37,6 @@ public class Excel {
             int minusColumIndex = 0;
 
             for (Row row : sheet) {
-                String firstname;
-                String lastname;
                 int cnt = 0;
                 List<String> rowValues = new ArrayList<>();
                 for (Cell cell : row) {
@@ -50,49 +53,46 @@ public class Excel {
                     }
                     cnt++;
                 }
-                firstname = rowValues.get(1);
-                lastname = rowValues.get(0);
-                mitarbeiterList.add(new Mitarbeiter(firstname, lastname, 0.0, 0.0,
+                mitarbeiterList.add(new Mitarbeiter(rowValues.get(1), rowValues.get(0), 0.0, 0.0,
                         null, rowValues.subList(2, rowValues.size())));
 
                 rows.add(rowValues);
             }
             workbook.close();
 
-            // Die ersten zwei einträge entfernen da das keine Mitarbeiter sind
+            weekdays = mitarbeiterList.get(1).getCalenderDays();
+            // Die ersten zwei Zeilen entfernen da das keine Mitarbeiter sind
             mitarbeiterList = mitarbeiterList.subList(2, mitarbeiterList.size());
-            rows.forEach(r -> System.out.println(r + "\n"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        createNewExcelFile(excelFile);
-        //writeContentInExcelList(rows);
+        try {
+            writeToExcel(rows);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         assert rows != null;
         return rows.toString();
     }
 
-    public void writeContentInExcelList(List<List<String>> rows){
+    public void writeToExcel(List<List<String>> rows) throws IOException {
+        String filePath = "src/main/resources/Excel/FertigerPlan.xlsx";
         Workbook workbook = new XSSFWorkbook();
-        Sheet sheet = workbook.getSheetAt(0);
-        Row row = sheet.createRow(0);
-        Cell cell = row.createCell(0);
-        cell.setCellValue("Hello, World!");
+        Sheet sheet = workbook.createSheet("Sheet1");
 
-        try (FileOutputStream outputStream = new FileOutputStream("src/main/resources/Excel/Wichtige_Dinge_Dienstplan.xlsx")) {
-            workbook.write(outputStream);
-        } catch (Exception e) {
-            e.printStackTrace();
+        int rowIndex = 0;
+        for (List<String> row : rows) {
+            Row newRow = sheet.createRow(rowIndex++);
+            int columnIndex = 0;
+            for (String value : row) {
+                Cell cell = newRow.createCell(columnIndex++);
+                cell.setCellValue(value);
+            }
         }
-    }
 
-    public void createNewExcelFile(File excelFile){
-        Path newFile = Path.of("src/main/resources/Excel/FertigerPlan.xlsx");
-
-        try {
-            Files.deleteIfExists(newFile);
-            Files.copy(excelFile.toPath(), newFile);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        FileOutputStream outputStream = new FileOutputStream(filePath);
+        workbook.write(outputStream);
+        workbook.close();
+        outputStream.close();
     }
 }
