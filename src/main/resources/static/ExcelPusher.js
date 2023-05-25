7bdb60744da15330524ef12a33a50877df72db0c
pieth252 <67559476+pieth252@users.noreply.github.com>
2023-05-25 14:20:28 +0200

    Download Excel java function

--- src/main/resources/static/ExcelPusher.js
+++ src/main/resources/static/ExcelPusher.js
@@ -1,9 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // @ts-ignore
-import * as XLSX from 'xlsx';
-const getExcelList = () => {
-    const url = '/api/excel/get';
+var XLSX = require("xlsx");
+var getExcelList = function () {
+    var url = '/api/excel/get';
     fetch(url)
-        .then(res => {
+        .then(function (res) {
         if (!res.ok) {
             document.getElementById('errorMsg').innerHTML = "GET request failed! HTTP-Status: " + res.status;
             throw Error("fetch failed!");
@@ -11,34 +13,34 @@ const getExcelList = () => {
         document.getElementById('errorMsg').innerHTML = "GET request succeeded!";
         return res.json();
     })
-        .then(res => {
+        .then(function (res) {
         console.log("HIER IST DIE LISTE");
         console.log(res);
     })
-        .catch(err => console.log(err));
+        .catch(function (err) { return console.log(err); });
 };
 function pushFile(blob) {
     console.log("working");
-    const fileReader = new FileReader();
+    var fileReader = new FileReader();
     fileReader.readAsArrayBuffer(blob);
-    fileReader.onload = (event) => {
-        const data = event.target.result;
-        const workbook = XLSX.read(data, { type: 'binary' });
+    fileReader.onload = function (event) {
+        var data = event.target.result;
+        var workbook = XLSX.read(data, { type: 'binary' });
         // Convert the Excel file to a JSON object
-        const sheetName = workbook.SheetNames[0];
-        const sheet = workbook.Sheets[sheetName];
-        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+        var sheetName = workbook.SheetNames[0];
+        var sheet = workbook.Sheets[sheetName];
+        var jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
         console.log(jsonData);
-        const requestOptions = {
+        var requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(jsonData)
         };
         fetch('http://localhost:8080/api/excel', requestOptions)
-            .then(response => {
+            .then(function (response) {
             alert('File uploaded successfully!');
         })
-            .catch(error => {
+            .catch(function (error) {
             alert('File upload failed.');
             console.error(error);
         });
